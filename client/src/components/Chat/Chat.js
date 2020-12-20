8e0e54a0dc0f1fafe89ad75d7a9c7d59df53f34e
snigdha <chaudhrysnigdha8@gmail.com>
2020-12-20 16:15:55 +0530

    'commit'

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "https://react-realtime-chatapplication.herokuapp.com/";
+  const ENDPOINT = "https://reactjs-realtime-chatapp.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
